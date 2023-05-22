ea2a8d9dee4e852b1630e29101765ca676fb7478
johnson feranmi <olamilekanferanmi99@gmail.com>
2023-05-22 19:56:21 +0100

    Jumia.com

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     let mycart = []
-    let doll= [];
-    let mybalance = 0
+    // console.log(mycart.length);
+    let mybalance = 0;
     let storageData;
     if(localStorage.mybalance){
         storageData = JSON.parse(localStorage.getItem("mybalance"));
@@ -11,92 +11,79 @@
 
     if (localStorage.cart) {
         mycart = JSON.parse(localStorage.getItem("cart")) 
+        console.log(mycart.length);
+        // lenght.textContent = mycart.length
     }
-    // const addtocart = (product)=>{
-    //     console.log(storageData);
-    //     // let imaga = document.getElementById("images").innerHTML;
-    //     // let name = document.getElementById("names").innerHTML;
-    //     // let price = document.getElementById("prices").innerHTML;
-        
-    //     let imagess = product.image;
-    //     let names = product.name;
-    //     let pricess = product.price
-        
-    //     let myitem = {
-    //         image:imagess,
-    //         name:names,
-    //         price:pricess
-    //     }
-    //     mycart.push(myitem)
-    //     localStorage.setItem("cart", JSON.stringify(mycart));
+    const laoded = ()=>{
+        let alluser = JSON.parse(localStorage.getItem("cart")) 
+        lenght.textContent = alluser.length;
+        setInterval(displayTime, 1000);
 
-        
-    //     if (localStorage.myhistory) {
-    //         doll = JSON.parse(localStorage.getItem("mybalance")) 
-            
-    //     }
-    //     if(!storageData){
-    //         mybalance + myitem.price;
-    //     }else{
-    //         let totalBalace = storageData + myitem.price;
-    //         console.log(totalBalace);
-    //         localStorage.setItem("mybalance", JSON.stringify(totalBalace));
-    //     }
-        
-        
-        
-    // }
-        const addToCart = (e) =>{
-            let imagess = e.image;
-            let names = e.name;
-            let pricess = e.price
-            
-            let myitem = {
-                image:imagess,
-                name:names,
-                price:pricess
-            }
-            mycart.push(myitem)
-            localStorage.setItem("cart", JSON.stringify(mycart));
+    };
 
-            let initialBalance = e.price;
-            if(!localStorage.totalBalance){
-                console.log(e);
-                localStorage.setItem("totalBalance", JSON.stringify(initialBalance))
-            }else{
-                initialBalance = JSON.parse(localStorage.getItem("totalBalance"))
-                initialBalance = initialBalance + e.price;
-                console.log(initialBalance);
-                localStorage.setItem("totalBalance", JSON.stringify(initialBalance))
-            }
-            let localData = JSON.parse(localStorage.getItem("totalBalance"))
-
+    const displayTime = ()=> {
+        let currentTime = new Date();
+        let hours = currentTime.getHours();
+        let minutes = currentTime.getMinutes();
+        let seconds = currentTime.getSeconds();
+        minutes = (minutes < 10 ? "0" : "") + minutes;
+        seconds = (seconds < 10 ? "0" : "") + seconds;
+        let period = hours >= 12 ? "PM" : "AM";
+        hours = hours % 12 || 12;
+        let timeString = hours + ":" + minutes + ":" + seconds + " " + period;
+        document.getElementById("clock").innerHTML = timeString;
+    };
+    const addToCart = (e) =>{
+        let images = e.image;
+        let names = e.name;
+        let prices = e.price
+        
+        let myitem = {
+            image:images,
+            name:names,
+            price:prices
         }
+        showimage.src = images;
+        showname.innerHTML = names;
+        showprice.innerHTML = `₦${prices}`
+
+        mycart.push(myitem)
+        localStorage.setItem("cart", JSON.stringify(mycart));
+
+        let initialBalance = e.price;
+        if(!localStorage.totalBalance){
+            console.log(e);
+            localStorage.setItem("totalBalance", JSON.stringify(initialBalance))
+        }else{
+            initialBalance = JSON.parse(localStorage.getItem("totalBalance"))
+            initialBalance = initialBalance + e.price;
+            console.log(initialBalance);
+            localStorage.setItem("totalBalance", JSON.stringify(initialBalance))
+        }
+        let localData = JSON.parse(localStorage.getItem("totalBalance"))
+
+    }
     const cart = ()=> {
         let alluser = JSON.parse(localStorage.getItem("cart")) 
-        disp.innerHTML +=  alluser.map((value,index) =>{
-            
-            return `<div class="col-md-5 col-12 col-sm-6 py-1">
-                        <div class="card">
-                        <p><span class="fw-bold ">${index + 1}</span> </p>
-                            <div class="d-inline d-lg-flex justify-content-between p-2 ">
-                                <div class="text-center">
-                                    <img alt="" class="img-fluid" width="160" src=${value.image} >
-                                </div>
-                                <div class="px-3 text-center">
-                                    <p><span class="fw-bold">${value.name}</span> </p>
-                                    <p><span class="fw-bold">₦ ${value.price}</p>
-                                </div>
-                                <div class="d-flex">
-                                    <button class="btn btn-outline-danger float-end fw-bold">Payment Method</button>
-                                    <button class="btn btn-outline-danger float-end fw-bold ms-2" onclick="Delete(${index})">Delete Item</button>
-                                </div>
-                            </div>
-                        </div> 
-                    </div>`
-            
+        lenght.textContent = alluser.length;
+        disp.innerHTML = "";
+        alluser.map((value, index) => {
+            disp.innerHTML +=`<div  data-bs-toggle="modal" data-bs-target="#exampleModal"  class="col-md-3 col-sm-6 col-6  py-1">
+            <div class="card">
+                <div class="d-inline d-lg-flex justify-content-between p-1 ">
+                    <div class="text-center">
+                        <img alt="" class="img-fluid rounded" width="400" src="${value.image}" >
+                    </div>
+                    <div class="px-3 text-center">
+                        <p><span class="fw-bold">${value.name}</span> </p>
+                        <p><span class="fw-bold">₦ ${value.price}</p>
+                    </div>
+                   
+                </div>
+            </div> 
+        </div>`
         })
-        console.log(alluser)
+        // console.log(alluser)
         mytotal.innerHTML = JSON.parse(localStorage.getItem("totalBalance"))
     }
     const cartt = ()=>{
@@ -105,12 +92,13 @@
     const home = ()=>{
         window.location.href ="index.html"
     }
-    const deleteall = (e)=>{
+    const deleteall = ()=>{
         // window.location.href ="index.html"
         let allusers = JSON.parse(localStorage.getItem("cart")) 
-        allusers.splice(e, allusers);
+        allusers.splice(allusers, allusers.length);
         console.log(allusers);
         localStorage.setItem("cart", JSON.stringify(allusers));
+        cart()
     }
     const Delete = (e)=>{
         let alluser = JSON.parse(localStorage.getItem("cart")) 
@@ -132,3 +120,6 @@
         location.reload()
         mycart()
     }
+
+
+
